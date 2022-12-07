ef8c0b81dfa35e09f11c8831638c4d8314e5dd88
CharlotteVaux <116537524+CharlotteVaux@users.noreply.github.com>
2022-12-07 13:05:07 +0000

    Added search engine

--- src/index.js
+++ src/index.js
@@ -40,8 +40,21 @@ function displayTemperature(response) {
     iconElement.setAttribute("alt",response.data.weather[0].description)
 }
 
-let apiKey = "fb53e9f0cdf1fede8235fdb56573a63e";
-let city = "Sydney";
+function search(city) {
+    let apiKey = "fb53e9f0cdf1fede8235fdb56573a63e";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-
 axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event){
+    event.preventDefault();
+    let cityInputElement = document.querySelector("#city-input");
+    search(cityInputElement.value);
+}
+
+search("Perth");
+
+
+
+let form= document.querySelector("#search-form");
+form.addEventListener("submit",handleSubmit);
